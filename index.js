d57020735240256d48c53a8dc7dd339b4637085c
Nikolay Nozdrin-Plotnitsky <nozdrin.plotnitsky@karma.red>
2018-12-04 11:21:31 +0300

    added send message feature

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 #!/usr/bin/env node
 
-const
-  { init, subscribe } = require("@karmared/kafka-transport"),
-  logger = topic => data => console.log(`\x1b[36m${topic}:\x1b[0m ${JSON.stringify(data)}`);
 
-start()
+const 
+  repl = require('repl'),
+  uuid = require('uuid/v4'),
+  chalk = require('chalk'),
+  { init, push, fetch, subscribe } = require("@karmared/kafka-transport"),
+  logger = topic => data => console.log(`${chalk.cyan(topic)}: ${JSON.stringify(data)}`);
+
+
+function initializeContext(context) {
+  let
+    topic,
+    host = process.argv.includes("--host")
+      ? process.argv[process.argv.indexOf("--host") + 1]
+      : "localhost:9092";
+
+  init({connectionString: host})
+
+  //context.init = init
+  context.push = (...args) => {push(...args)}
+  context.fetch = fetch
+  context.subscribe = (...args) => {subscribe(...args)}
+  
+  context.topic = _topic => {
+    topic = _topic
+    r.setPrompt(`${chalk.green(topic)}> `)
+  }
+
+  context.send = data => {
+    push(topic, data, uuid())
+  }
+
+  if (process.argv.includes("--topics"))
+    start()
+}
+
+
+const r = repl.start({ prompt: '> ' });
+initializeContext(r.context);
+
+
+r.on('reset', initializeContext);
+
 
 async function start() {
   let
-    host = process.argv.includes("--host")
-      ? process.argv[process.argv.indexOf("--host") + 1]
-      : "localhost:9092",
     topics = process.argv.includes("--topics")
       ? process.argv[process.argv.indexOf("--topics") + 1].split(",")
       : ["test"]
 
-  init({connectionString: host})
   topics.forEach(
     topic => subscribe(topic, logger(topic))
   )
